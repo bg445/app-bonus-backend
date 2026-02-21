5c45d2e480b228c7a55ea8facb30c2ed45487c4c
Brou Gerard <kouassibrougerard8@gmail.com>
2026-02-21 23:20:16 +0000

    fix railway port

--- server.js
+++ server.js
@@ -121,4 +121,8 @@ app.post("/add-user", (req, res) => {
 });
 
 // ----------------- Lancer le serveur -----------------
-app.listen(5000, () => console.log("Backend lancé sur http://localhost:5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Backend lancé sur le port ${PORT}`);
+});
